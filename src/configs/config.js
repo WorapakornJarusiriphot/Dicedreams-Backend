f6c94e5930c519819c0538eb8c4f03eb6ebd20bd
worapakorn <120018217+WorapakornJarusiriphot@users.noreply.github.com>
2024-10-08 14:57:38 +0700

    Refactor config.js to add IMAGE_PATH environment variable

--- src/configs/config.js
+++ src/configs/config.js
@@ -6,4 +6,5 @@ module.exports = {
   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
   AWS_REGION: process.env.AWS_REGION,
   S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
+  IMAGE_PATH: process.env.IMAGE_PATH,
 };
